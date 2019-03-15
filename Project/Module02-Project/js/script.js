97760589fc08435a830d12dadfb19655d6c949ba
ShaunHarmon <shaun.harmon@gmail.com>
2019-03-15 09:15:06 -0500

    Update script.js

--- Project/Module02-Project/js/script.js
+++ Project/Module02-Project/js/script.js
@@ -1,24 +1,16 @@
 window.addEventListener('load', function () {
    
     
-      // Let's check whether notification permissions have already been granted
-       if (Notification.permission === "granted") {
-        // If it's okay let's create a notification
-        var notification = new Notification("Hi there!");
-      }
-    
-      // Otherwise, we need to ask the user for permission
-      else if (Notification.permission !== "denied") {
-        Notification.requestPermission().then(function (permission) {
-          // If the user accepts, let's create a notification
-          if (permission === "granted") {
-            var notification = new Notification("Hi there!");
-          }
-        });
-      }
-    
-      // At last, if the user has denied notifications, and you 
-      // want to be respectful there is no need to bother them any more.
-    
+  Notification.requestPermission().then(function(result) {
+    if (result === 'denied') {
+      console.log('Permission wasn\'t granted. Allow a retry.');
+      return;
+    }
+    if (result === 'default') {
+      console.log('The permission request was dismissed.');
+      return;
+    }
+    // Do something with the granted permission.
+  });
 
   });
