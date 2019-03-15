e5556fa3e357c7bb959e9030e1950fa6f60df83b
ShaunHarmon <shaun.harmon@gmail.com>
2019-03-15 09:33:09 -0500

    Update script.js

--- Project/Module02-Project/js/script.js
+++ Project/Module02-Project/js/script.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', function () {
-
-    Notification.requestPermission(function(result) {
-      if (result === 'denied') {
-        console.log('Permission wasn\'t granted. Allow a retry.');
-        return;
-      }
-      if (result === 'default') {
-        console.log('The permission request was dismissed.');
-        return;
-      }  
-    });
-
-    if(Notification.permission === 'granted'){
-      var notification = new Notification("NEW GALLERY!"); 
+   
+    
+  Notification.requestPermission(function(result) {
+    if (result === 'denied') {
+      console.log('Permission wasn\'t granted. Allow a retry.');
+      
     }
+    if (result === 'default') {
+      console.log('The permission request was dismissed.');
+    
+    }
+    if(result === 'granted'){
+      var notification = new Notification("NEW GALLERY!");   
+    }
+  });
 
   });
